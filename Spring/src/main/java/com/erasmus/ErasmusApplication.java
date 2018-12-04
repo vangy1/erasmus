8417c7ab2cff6802c6f53a1ff2fb0fc1d8737155
vangor <robert.vangy@gmail.com>
2018-12-04 10:14:27 +0100

    Initial setup

---
+++ Spring/src/main/java/com/erasmus/ErasmusApplication.java
@@ -0,0 +1,12 @@
+package com.erasmus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErasmusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErasmusApplication.class, args);
+    }
+}
